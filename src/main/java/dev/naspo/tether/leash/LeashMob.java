73668974f566061bfb952f3a0952a745f864f6ee
Athanasios Topaltsis <a.topaltsis@gmail.com>
2025-04-09 14:57:18 -0400

    Added whitelist mob feature. | Fixed blacklist not working.

--- src/main/java/dev/naspo/tether/leash/LeashMob.java
+++ src/main/java/dev/naspo/tether/leash/LeashMob.java
@@ -3,17 +3,20 @@ package dev.naspo.tether.leash;
 import dev.naspo.tether.core.Tether;
 import dev.naspo.tether.core.Utils;
 import org.bukkit.*;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.PlayerLeashEntityEvent;
 import org.bukkit.event.player.PlayerInteractAtEntityEvent;
 import org.bukkit.inventory.EquipmentSlot;
 import org.bukkit.inventory.ItemStack;
 
-public class LeashMob implements Listener {
-    private LivingEntity clicked;
+import java.util.List;
+import java.util.stream.Collectors;
 
+public class LeashMob implements Listener {
     private ClaimCheckManager claimCheckManager;
     private Tether plugin;
 
@@ -22,15 +25,19 @@ public class LeashMob implements Listener {
         this.claimCheckManager = claimCheckManager;
     }
 
-    // Leashing the mob, general event.
+    // A more general event than PlayerLeashEntityEvent, used for leashing mobs that
+    // are not leasable by default.
     @EventHandler
     public void onInteract(PlayerInteractAtEntityEvent event) {
         Player player = event.getPlayer();
+        LivingEntity clicked;
 
         if (event.getRightClicked() instanceof LivingEntity) {
+            // If it was a player that was right-clicked, return.
             if (event.getRightClicked() instanceof Player) {
                 return;
             }
+            // Capture the clicked entity.
             clicked = (LivingEntity) event.getRightClicked();
 
             if (event.getHand() == EquipmentSlot.OFF_HAND) {
@@ -45,6 +52,7 @@ public class LeashMob implements Listener {
                 return;
             }
 
+            // Permission check.
             if (!(player.hasPermission("tether.use"))) {
                 return;
             }
@@ -54,6 +62,11 @@ public class LeashMob implements Listener {
                 event.setCancelled(true);
                 player.sendMessage(Utils.chatColor(Utils.prefix + plugin.getConfig().getString(
                         "messages.in-claim-deny-mob")));
+                return;
+            }
+
+            // Checking if clicked entity passes blacklist/whitelist check.
+            if (isEntityRestricted(clicked)) {
                 return;
             }
 
@@ -80,5 +93,43 @@ public class LeashMob implements Listener {
                 }, 1L);
             }
         }
+    }
+
+    // Specific leash event to apply blacklist/whitelist to mobs that are leashable
+    // in the base game. This is not possible to catch with the PlayerInteractAtEntityEvent.
+    @EventHandler
+    public void onLeash(PlayerLeashEntityEvent event) {
+        Entity entity = event.getEntity();
+
+        // Checking if clicked entity passes blacklist/whitelist check.
+        if (isEntityRestricted(entity)) {
+            event.setCancelled(true);
+        }
+    }
+
+    // Checks the whitelist or blacklist to see whether the entity is restricted from being leashed or not.
+    private boolean isEntityRestricted(Entity entity) {
+        // Use whitelist check.
+        // If whitelist is set to be used over blacklist, check the whitelist only, else use blacklist.
+        if (plugin.getConfig().getBoolean("use-whitelist-over-blacklist")) {
+            // Whitelist check.
+            // Getting whitelist values and converting all to uppercase.
+            List<String> whitelist = plugin.getConfig().getStringList("whitelisted-mobs")
+                    .stream().map(String::toUpperCase).collect(Collectors.toList());
+
+            if (!whitelist.contains(entity.getType().name())) {
+                return true;
+            }
+        } else {
+            // Blacklist check.
+            // Getting blacklist values and converting all to uppercase.
+            List<String> blacklist = plugin.getConfig().getStringList("blacklisted-mobs")
+                    .stream().map(String::toUpperCase).collect(Collectors.toList());
+
+            if (blacklist.contains(entity.getType().name())) {
+                return true;
+            }
+        }
+        return false;
     }
 }
