bc9405f865aca06ba48ee223afad17caacee48d1
Athanasios <a.topaltsis@gmail.com>
2026-05-06 23:17:12 -0400

    WorldGuard integration rework
    Tether's custom LEASH flag now respects the default INTERACT flag, which in turn respects the BUILD flag. However in the __global__ region, only BUILD is checked.

--- src/main/java/dev/naspo/tether/integrations/standardintegrations/WorldGuardIntegration.java
+++ src/main/java/dev/naspo/tether/integrations/standardintegrations/WorldGuardIntegration.java
@@ -4,18 +4,21 @@ import com.sk89q.worldedit.bukkit.BukkitAdapter;
 import com.sk89q.worldguard.LocalPlayer;
 import com.sk89q.worldguard.WorldGuard;
 import com.sk89q.worldguard.bukkit.WorldGuardPlugin;
+import com.sk89q.worldguard.protection.ApplicableRegionSet;
 import com.sk89q.worldguard.protection.flags.Flag;
 import com.sk89q.worldguard.protection.flags.Flags;
 import com.sk89q.worldguard.protection.flags.StateFlag;
 import com.sk89q.worldguard.protection.flags.registry.FlagConflictException;
 import com.sk89q.worldguard.protection.flags.registry.FlagRegistry;
+import com.sk89q.worldguard.protection.regions.ProtectedRegion;
 import com.sk89q.worldguard.protection.regions.RegionContainer;
 import dev.naspo.tether.Tether;
 import dev.naspo.tether.integrations.Integration;
 import dev.naspo.tether.integrations.IntegrationEnablePhase;
-import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
+
+import java.util.Set;
 
 public class WorldGuardIntegration extends Integration {
     private WorldGuard worldGuardAPI;
@@ -54,27 +57,50 @@ public class WorldGuardIntegration extends Integration {
         // Region data can be accessed via the RegionContainer object.
         RegionContainer regionContainer = worldGuardAPI.getPlatform().getRegionContainer();
 
-        // Query for the state of the default "interact" flag and Tether's custom "leash" flag.
+        // Query for the applicable regions.
+        ApplicableRegionSet applicableRegionSet = regionContainer.createQuery().getApplicableRegions(wgLocation);
+        Set<ProtectedRegion> protectedRegions = applicableRegionSet.getRegions();
+
+        // Query for the state of the default BUILD and INTERACT flags, and Tether's custom LEASH flag.
+        // This is needed because Tether's LEASH flag should respect BUILD and INTERACT.
+        boolean build = regionContainer.createQuery().testBuild(wgLocation, wgLocalPlayer);
         boolean interact = regionContainer.createQuery().testState(wgLocation, wgLocalPlayer, Flags.INTERACT);
         boolean leash = regionContainer.createQuery().testState(wgLocation, wgLocalPlayer, leashFlag);
 
-        // First check the "interact" flag, as Tether's custom "leash" flag should respect the "interact" flag first.
-        // If it's false, deny the leash.
-        if (!interact) {
-            Bukkit.getServer().broadcastMessage("wg interact flag is false, denying");
-            return false;
-        }
-        Bukkit.getServer().broadcastMessage("wg interact flag is true");
+        /*
+        Note on calculating an outcome based on the state of different flags:
 
-        //TEMP TODO: remove this
-        if (leash) {
-            Bukkit.getServer().broadcastMessage("wg leash flag is true");
+        As per WorldGuard definition, flags like INTERACT "are used in tandem with the BUILD flag - if the player can
+        build, then these flags do not need to be checked (although they are still checked for DENY), so they
+        are false by default." - comment in com.sk89q.worldguard.protection.flags.Flags
+
+        This means that if BUILD is set to ALLOW (true), then we can assume that flags like INTERACT are also allowed
+        and therefore shouldn't manually check them. (If we try and check these flags manually anyway, they will return false/DENY).
+
+        If BUILD is set to DENY (false), then we can check to see if these other sub-flags like INTERACT are allowed.
+
+        The reason that we needed to note this down for Tether is because Tether needs to do manual flag calculation
+        for its custom LEASH flag.
+         */
+
+        // If protectedRegions is empty, then we are in the __global__ region.
+        if (protectedRegions.isEmpty()) {
+            // In the __global__ region, I am only checking for the BUILD flag.
+            return build;
+        }
+
+        // protectedRegions is NOT empty, we are in a user defined region.
+
+        if (build) {
+            // If BUILD is allowed, then INTERACT is also assumed as per worldguard definitions, so just check LEASH.
+            return leash;
         } else {
-            Bukkit.getServer().broadcastMessage("wg leash flag is false");
+            // Otherwise if BUILD is not allowed, then check INTERACT, then LEASH.
+            if (!interact) {
+                return false;
+            }
+            return leash;
         }
-
-        // At this point the "interact" flag is true, now the leash operation depends on the state of the leash flag.
-        return leash;
     }
 
     /**
