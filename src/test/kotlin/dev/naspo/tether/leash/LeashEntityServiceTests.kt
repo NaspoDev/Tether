93c2e9ac65008b86dbc68e24489c3c2f0e4cb4fc
AthanasiosT <a.topaltsis@gmail.com>
2026-08-16 13:46:28 -0400

    Working on LeashEntityServiceTests

--- src/test/kotlin/dev/naspo/tether/leash/LeashEntityServiceTests.kt
+++ src/test/kotlin/dev/naspo/tether/leash/LeashEntityServiceTests.kt
@@ -1,8 +1,56 @@
 package dev.naspo.tether.leash
 
+import dev.naspo.tether.Tether
+import dev.naspo.tether.config.ConfigAccessor
+import dev.naspo.tether.integrations.IntegrationManager
+import org.bukkit.Location
+import org.bukkit.World
+import org.bukkit.entity.Cow
+import org.bukkit.entity.EntityType
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertDoesNotThrow
+import org.mockbukkit.mockbukkit.MockBukkit
 import org.mockbukkit.mockbukkit.ServerMock
+import org.mockbukkit.mockbukkit.entity.PlayerMock
+import org.mockbukkit.mockbukkit.world.WorldMock
+
 
 class LeashEntityServiceTests {
 
-    private lateinit var serverMock: ServerMock
+    private lateinit var server: ServerMock
+    private lateinit var plugin: Tether
+    private lateinit var configAccessor: ConfigAccessor
+    private lateinit var integrationManager: IntegrationManager
+    private lateinit var leashEntityService: LeashEntityService
+    private lateinit var world: World
+    private lateinit var player: PlayerMock
+
+    @BeforeEach
+    fun setUp() {
+        server = MockBukkit.mock()
+        plugin = MockBukkit.load(Tether::class.java)
+        configAccessor = ConfigAccessor(plugin)
+        integrationManager = IntegrationManager(plugin, configAccessor)
+        leashEntityService = LeashEntityService(plugin, configAccessor, integrationManager)
+        world = WorldMock()
+        player = server.addPlayer()
+    }
+
+    @AfterEach
+    fun tearDown() {
+        MockBukkit.unmock()
+    }
+
+    // Note: DLE stands for "Default Leashable Entity"
+
+    @Test
+    fun `test leashEntityToPlayer with DLE`() {
+        var location = Location(world,  0.0, 64.0, 0.0)
+        var cow = world.spawnEntity(location, EntityType.COW)
+        assertDoesNotThrow {
+            leashEntityService.leashEntityToPlayer(player, cow)
+        }
+    }
 }
