96aac297e56c2e9b664b4f8ffd2fae65ec842e25
AthanasiosT <a.topaltsis@gmail.com>
2026-08-29 13:21:08 -0400

    First rendition of ProxyEntity

--- src/main/kotlin/dev/naspo/tether/leash/playerleash/ProxyEntity.kt
+++ src/main/kotlin/dev/naspo/tether/leash/playerleash/ProxyEntity.kt
@@ -1,87 +1,115 @@
 package dev.naspo.tether.leash.playerleash
 
 import dev.naspo.tether.Tether
-import io.papermc.paper.entity.Leashable
 import org.bukkit.Location
 import org.bukkit.NamespacedKey
 import org.bukkit.World
-import org.bukkit.entity.Entity
-import org.bukkit.entity.EntityType
 import org.bukkit.entity.LivingEntity
 import org.bukkit.entity.Player
 import org.bukkit.entity.Zombie
 import org.bukkit.persistence.PersistentDataType
+import org.bukkit.scheduler.BukkitTask
 
 /**
  * The special invisible entity used in player leashing.
  *
- * @property player The player for which this entity is the proxy of.
+ * ### How It Works
+ * The proxy entity is a special invisible, invulnerable, no AI entity. It was what the leash holder actually leashes,
+ * and the proxied player is constantly teleported to this entity every tick.
+ * This is how player leashing actually works.
+ *
+ * ### Upon Initialization
+ * - The proxy entity will be spawned (with its special properties) at the proxied player's location.
+ * - The proxy entity, and therefore its proxied player, will be leashed to the leash holder.
+ * - The proxied player will be constantly teleported to this entity every tick.
+ *
+ * ### Properties
+ * @property proxiedPlayer The player for which this entity is the proxy of.
+ * @property leashHolder The leash holder of this proxy entity, and therefore also it's proxied player.
  * @property plugin The plugin instance.
  */
 class ProxyEntity(
-    private val player: Player,
+    private val proxiedPlayer: Player,
+    private val leashHolder: Player,
     private val plugin: Tether
 ) {
     companion object {
-        private val entityType = EntityType.ZOMBIE
+        private val entityType: Class<Zombie> = Zombie::class.java
         private const val PDC_KEY = "naspodev_tether_playerleash_proxy_entity"
     }
 
     // The actual backing entity.
-    private var entity: LivingEntity? = null
+    private val entity: LivingEntity = spawn()
     // NamespacedKey for Persistent Data Container
     private val namespacedKey = NamespacedKey(plugin, PDC_KEY)
+    // The repeating task to teleport the proxied player to the proxy entity every tick.
+    private var teleportationTask: BukkitTask? = null
 
-    /**
-     * Spawn the proxy entity at its player's location.
-     * This will not spawn multiple entities if called multiple times.
-     */
-    fun spawn() {
-        // If its already spawned do nothing.
-        if (entity != null) return
-
-        val location: Location = player.location
-        val world: World = player.world
-
-        entity = world.spawnEntity(location, entityType) as LivingEntity
-
-        attachPDC()
-        configureAttributes()
+    init {
+        entity.setLeashHolder(leashHolder)
+        startTeleportationTask()
     }
 
     /**
      * Leashes this entity, and therefore its proxied player, to a player.
      *
-     * ### How It Works
-     * The proxied player (player associated with this Proxy Entity) will be
-     * constantly teleported to this entity every tick. This is how player leashing actually works.
+     * Note that upon intialization of ProxyEntity, it will already be leashed to the provided leash holder.
+     * This method is indended to be used to leash this proxy entity to a different leash holder, or to unleash it.
      *
-     * ### Params
      * @param player The player to be the leash holder of this proxy (and its proxied player).
      * If `null`, this will unleash and delete the proxy.
      */
     fun setLeashHolder(player: Player?) {
-        val entity = entity ?: return
+        entity.setLeashHolder(player)
 
-        // TODO: implement this
+        // If the entity is not leashed, i.e. player was null, then destroy this proxy entity.
+        if (!entity.isLeashed) {
+            destroy()
+        }
     }
 
     // --- Private ---
 
+    /**
+     * Spawn the proxy entity at its player's location.
+     */
+    private fun spawn(): LivingEntity {
+        val location: Location = proxiedPlayer.location
+        val world: World = proxiedPlayer.world
+
+        return world.spawn(location, entityType) {
+            attachPDC(it)
+            configureAttributes(it)
+        }
+    }
+
     // Creates and attaches a pdc to the entity, marking is as a special proxy entity.
-    private fun attachPDC() {
-        val entity = entity ?: return
+    private fun attachPDC(entity: LivingEntity) {
         val pdc = entity.persistentDataContainer
         pdc.set(namespacedKey, PersistentDataType.STRING, "_")
     }
 
     // Sets special properties on the entity. (Ex. invisible, no AI, etc...)
-    private fun configureAttributes() {
-        val entity = entity ?: return
+    private fun configureAttributes(entity: LivingEntity) {
         entity.isInvisible = true
         entity.isInvulnerable = true
         entity.isSilent = true
         entity.setAI(false)
     }
 
+    // Creates and starts a task to constatly teleport the proxied player to this proxy entity every tick.
+    private fun startTeleportationTask() {
+        teleportationTask = plugin.server.scheduler.runTaskTimer(plugin, Runnable {
+            proxiedPlayer.teleport(entity.location)
+        }, 0, 1)
+    }
+
+    /**
+     * Destroy this proxy entity.
+     * This will cancel the teleportation task and removes the entity.
+     */
+    private fun destroy() {
+        teleportationTask?.cancel()
+        entity.remove()
+    }
 }
